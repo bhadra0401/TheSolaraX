09bb7e23a9ce32dfb3983bd9c1b2d8f0b737148c
bhadra0401 <238x1a0503@khitguntur.ac.in>
2025-02-13 19:40:57 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById("login-password").value.trim();
 
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://thesolarax.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
